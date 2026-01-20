7cf7240ac03a69b86793b7c3a16f6cfd50821191
KRISHNAJI S <skrishnaji5@gmail.com>
2026-01-20 22:45:53 +0530

    Time: 0 ms (100.00%) | Memory: 21.4 MB (66.53%) - LeetSync

---
+++ 112-path-sum/path-sum.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool hasPathSum(TreeNode* root, int targetSum) {
+        if(root == NULL) return false;
+
+        targetSum -= root->val;
+
+        // if leaf node
+        if(root->left == NULL && root->right == NULL) {
+            return (targetSum == 0);
+        }
+
+        return hasPathSum(root->left, targetSum) || hasPathSum(root->right, targetSum);
+    }
+};
+
