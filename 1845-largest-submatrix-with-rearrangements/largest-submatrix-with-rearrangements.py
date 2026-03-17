495a7dba4ef1dcf0e7158b6ef7f13c05e9fbd57c
KRISHNAJI S <skrishnaji5@gmail.com>
2026-03-17 15:53:50 +0530

    Time: 272 ms (57.14%) | Memory: 32.9 MB (42.86%) - LeetSync

---
+++ 1845-largest-submatrix-with-rearrangements/largest-submatrix-with-rearrangements.py
@@ -0,0 +1,22 @@
+class Solution:
+    def largestSubmatrix(self, matrix):
+        m, n = len(matrix), len(matrix[0])
+        
+        # Step 1: Build heights
+        for i in range(1, m):
+            for j in range(n):
+                if matrix[i][j] == 1:
+                    matrix[i][j] += matrix[i-1][j]
+        
+        max_area = 0
+        
+        # Step 2 & 3: Sort each row and calculate area
+        for i in range(m):
+            row = sorted(matrix[i], reverse=True)
+            
+            for j in range(n):
+                height = row[j]
+                width = j + 1
+                max_area = max(max_area, height * width)
+        
+        return max_area
