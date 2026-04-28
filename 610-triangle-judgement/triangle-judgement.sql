f504da0b62afee0ebb199996c828f081be78525d
KRISHNAJI S <skrishnaji5@gmail.com>
2026-04-28 09:55:38 +0530

    Time: 328 ms (13.81%) | Memory: 0B (100.00%) - LeetSync

---
+++ 610-triangle-judgement/triangle-judgement.sql
@@ -0,0 +1,5 @@
+# Write your MySQL query statement below
+# Write your MySQL query statement below
+select *, case when x+y>z and x+z>y and y+z>x then "Yes" Else "No" 
+end as triangle
+from triangle
