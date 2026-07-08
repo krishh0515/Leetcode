efbe2c2ba861b76a8bd71e54620559f2b409bcbc
KRISHNAJI S <skrishnaji5@gmail.com>
2026-07-08 11:22:50 +0530

    Time: 3 ms (7.48%) | Memory: 12.4 MB (56.47%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -1,11 +1,14 @@
 class Solution(object):
-        def rob(self, nums):
-            n=len(nums)
-            if n==1:
-                return nums[0]
-            dp=[0]*n
-            dp[0]=nums[0]
-            dp[1]=max(nums[0],nums[1])
-            for i in range(2,n):
-                dp[i]=max(dp[i-1],nums[i]+dp[i-2])
-            return dp[-1]
+    def rob(self, nums):
+        n=len(nums)
+        dp=[-1]*n
+        def solve(i):
+            if i>=n:
+                return 0
+            if dp[i]!=-1:
+                return dp[i]
+            rob=nums[i]+solve(i+2)
+            skip=solve(i+1)
+            dp[i]=max(rob,skip)
+            return dp[i]
+        return solve(0)
